9828b87a658af1b107e3c5b121d3b8d4aa31ad32
A2 <apple@Anmols-MacBook-Pro.local>
2015-08-21 01:27:28 +0530

    initial commit

---
+++ evaluate.js
@@ -0,0 +1,11 @@
+var page = require('webpage').create();
+var url = "http://www.google.com";
+page.onConsoleMessage = function(msg) {
+  console.log('Page title is ' + msg);
+};
+page.open(url, function(status) {
+  page.evaluate(function() {
+    console.log(document.title);
+  });
+  phantom.exit();
+});
